6275e38d15555b743a47bb223f201f22ff77b465
zhaohaiqi <1273983098@qq.com>
2020-05-16 12:04:20 +0800

    Initial commit

---
+++ src/main/java/com/github/gitdemo/GitdemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitdemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/github/gitdemo/controller/Hello.java
@@ -0,0 +1,13 @@
+package com.github.gitdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("hello")
+    public String returnString(){
+        return "hello world";
+    }
+}
